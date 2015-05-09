1b648d001daad4703a91156d09024cdc0767a7c1
Eric Hsu <erichsu7@gmail.com>
2015-05-09 14:19:02 -0700

    Set up slideshow interval

--- js/slideshow.js
+++ js/slideshow.js
@@ -5,7 +5,20 @@
 
   var View = Slideshow.View = function ($el) {
     this.$el = $el;
+    this.currentPicIdx = 0;
   }
 
-  
+  View.PICTURE_URLS = [
+    "/img/01.jpg",
+    "/img/02.jpg",
+    "/img/03.jpg"
+  ];
+
+  View.prototype.start = function () {
+    window.setInterval(this.swapPictures, 3000);
+  };
+
+  View.prototype.swapPictures = function () {
+    console.log("hi")
+  }
 })();
