12d9a23076e622c3718c230483942584afb74abd
Eric Hsu <erichsu7@gmail.com>
2015-05-09 23:11:08 -0700

    Add text

--- js/slideshow.js
+++ js/slideshow.js
@@ -11,21 +11,35 @@
   View.PICTURE_URLS = [
     "img/01.jpg",
     "img/02.jpg",
-    "img/03.jpg"
+    "img/03.jpg",
+    "img/04.jpg",
+    "img/05.jpg",
+    "img/06.jpg",
+    "img/07.jpg",
+    "img/08.jpg",
+    "img/09.jpg",
+
   ];
 
   View.prototype.start = function () {
-    var $topImg = $("<img class=\"top\">");
+    var that = this;
+    var $topImg = $("<img class=\"top img-responsive\">");
     $topImg.attr("src", View.PICTURE_URLS[this.currentPicIdx]);
     this.$el.append($topImg);
+    $topImg.load(function (event) {
+      var picHeight = $topImg.height();
+      that.$el.height(picHeight);
+    });
+    this.listenForResize();
 
     window.setInterval(this.swapPictures.bind(this), 3000);
   };
 
   View.prototype.swapPictures = function () {
+    var that = this;
     var $topImg = $(".top");
     var nextPicIdx = (this.currentPicIdx + 1) % View.PICTURE_URLS.length;
-    var $bottomImg = $("<img class=\"bottom\">");
+    var $bottomImg = $("<img class=\"bottom img-responsive\">");
     $bottomImg.attr("src", View.PICTURE_URLS[nextPicIdx]);
     this.$el.append($bottomImg);
     $bottomImg.addClass("top").removeClass("bottom", 500);
@@ -34,5 +48,13 @@
     });
 
     this.currentPicIdx = nextPicIdx;
+  }
+
+  View.prototype.listenForResize = function () {
+    var that = this;
+    $(window).resize(function (event) {
+      var picHeight = $("img.top").height();
+      that.$el.height(picHeight);
+    })
   }
 })();
